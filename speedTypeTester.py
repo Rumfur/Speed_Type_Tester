4464b906194a2fb6bb54a958c8f4033432b243f6
Pukitis <rovsvg@gmail.com>
2021-09-29 10:08:05 +0300

    testing config file

--- speedTypeTester.py
+++ speedTypeTester.py
@@ -1,1 +1,19 @@
-print("Hello world!")
+from os.path import exists
+
+def writeConfig(): # Writes config file lines
+    file = open("config.txt", "a")
+    file.write("Testing writing!\n")
+    file.write("I wonder what i'll write here.")
+    file.close()
+
+def checkConfig():
+    if not(exists("config.txt")):
+        file = open("config.txt","x") # Makes config file, if doesn't exist
+        writeConfig()
+
+def readConfig():
+    file = open("config.txt", "r") # Reads config file
+    print(file.read())
+
+checkConfig()
+readConfig()
